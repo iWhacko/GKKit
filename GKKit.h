a6c15137aab815936a54c32c4c995e6787da6cb7
Gaurav Khanna <gauravk92@gmail.com>
2012-03-16 03:42:05 -0400

    moving away

--- GKKit.h
+++ GKKit.h
@@ -2,16 +2,15 @@
     #import "common.h"
     #import "debug.h"
     #import "singleton.h"
-    #import "GKEditorProtocol.h"
-    #import "GKGradientLayer.h"
-    #import "GKRoundedPageView.h"
-    #import "GKActivityView.h"
-    #import "GKSearchController.h"
     #import "NSDate+GKAdditions.h"
     #import "NSObject+GKAdditions.h"
     #import "NSString+GKAdditions.h"
     #import "View+GKAdditions.h"
     #if IPHONE_ONLY
+        #import "GKGradientLayer.h"
+        #import "GKRoundedPageView.h"
+        #import "GKSearchController.h"
+        #import "GKActivityView.h"
         #import "GKSearchController.h"
     #endif
 #endif
